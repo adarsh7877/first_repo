ab98af47a265ffe109c97084f899273c99a1f1e5
adarshyadav7877 <adarshkumar447844@gmail.com>
2025-12-30 20:06:10 +0530

    Add files via upload

---
+++ number_guessing_game.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    //Initialize random number generator
+    srand(time(0));
+    //generate random number between 1 and 100
+    int randomNumber=(rand()%100)+1;
+    int no_of_guesses=0;
+    int guessed;
+    //Print the random number
+    //printf("Random Number : %d\n",randomNumber);
+    do{
+        printf("Guess the number : ");
+        scanf("%d",&guessed);
+        if(guessed>randomNumber){
+            printf("lower number please!\n");
+        }
+        else if (randomNumber>guessed){
+            printf("higher number please!\n");
+        }
+        else{
+            printf("congrats!!\n");
+        }
+        no_of_guesses++;
+    }while(guessed!=randomNumber);
+    printf("You guessed the number in %d guesses,",no_of_guesses);
+    return 0;
+}
